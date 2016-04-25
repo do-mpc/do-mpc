f82288e380c603dadb994060e30562086ac67ae8
sergiolucia <sergiolucia15@gmail.com>
2016-04-25 15:49:04 +0200

    added first draft of new classes definitions

---
+++ code/setup_functions/core_do_mpc.py
@@ -0,0 +1,169 @@
+# 	 -*- coding: utf-8 -*-
+#
+#    This file is part of DO-MPC
+#
+#    DO-MPC: An environment for the easy, modular and efficient implementation of
+#            robust nonlinear model predictive control
+#
+#    The MIT License (MIT)
+#
+#    Copyright (c) 2014-2015 Sergio Lucia, Alexandru Tatulea-Codrean
+#                            TU Dortmund. All rights reserved
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+
+class ocp:
+    """ A class that contains a full description of the optimal control problem and will be used in the model class. This is dependent on a specific element of a model class"""
+    def __init__(self, param_dict, *opt):
+        # Initial state and initial input
+        self.x0 = param_dict["x0"]
+        self.u0 = param_dict["u0"]
+        # Bounds for the states
+        self.x_lb = param_dict["x_lb"]
+        self.x_ub = param_dict["x_ub"]
+        # Bounds for the inputs
+        self.u_lb = param_dict["u_lb"]
+        self.u_ub = param_dict["u_ub"]
+        # Scaling factors
+        self.x_scaling = param_dict["x_scaling"]
+        self.u_scaling = param_dict["u_scaling"]
+        # Symbolic nonlinear constraints
+        self.cons = param_dict["cons"]
+        # Upper bounds (no lower bounds for nonlinear constraints)
+        self.cons_ub = param_dict["cons_ub"]
+        # Terminal constraints
+        self.cons_terminal = param_dict["cons_terminal"]
+        self.cons_terminal_lb = param_dict["cons_terminal_lb"]
+        self.cons_terminal_ub = param_dict["cons_terminal_ub"]
+        # Flag for soft constraints
+        self.soft_constraints = param_dict["soft_constraints"]
+        # Penalty term and maximum violation of soft constraints
+        self.penalty_term_cons = param_dict["penalty_term_cons"]
+        self.maximum_violation = param_dict["maximum_violation"]
+        # Lagrange term, Mayer term, and term for input variations
+        self.lterm = param_dict["lterm"]
+        self.mterm = param_dict["mterm"]
+        self.rterm = param_dict["rterm"]
+
+class model:
+    """A class for the definition model equations and optimal control problem formulation"""
+    def __init__(self, param_dict, *opt):
+        # Assert for define length of param_dict
+        required_dimension = 24
+        try:
+            assert(len(param_dict) == required_dimension)
+        raise Exception("Model / OCP information is incomplete. The number of elements in the dictionary is not correct")
+        # Assign the main variables describing the model equations
+        self.x = param_dict["x"]
+        self.u = param_dict["u"]
+        self.p = param_dict["p"]
+        self.z = param_dict["z"]
+        self.rhs = param_dict["rhs"] # Right hand side of the DAE equations
+         # Assign the main variables that describe the OCP
+        self.ocp = ocp(param_dict)
+
+    @clasmethod
+    def user_model(cls, param_dict, *opt):
+        " This is open for the implementation of a user-defined model class"
+        dummy = 1
+        return cls(dummy)
+
+class simulator:
+    """A class for the definition model equations and optimal control problem formulation"""
+    def __init__(self, model_simulator, param_dict, *opt):
+        # Assert for define length of param_dict
+        required_dimension = 7
+        try:
+            assert(len(param_dict) == required_dimension)
+        raise Exception("Simulator information is incomplete. The number of elements in the dictionary is not correct")
+        self.simulator = param_dict["simulator"]
+        self.plot_states = param_dict["plot_states"]
+        self.plot_control = param_dict["plot_control"]
+        self.plot_anim = param_dict["plot_anim"]
+        self.export_to_matlab = param_dict["export_to_matlab"]
+        self.export_name = param_dict["export_name"]
+        self.p_real = param_dic["p_real"]
+
+    @clasmethod
+    def user_simulator(cls, param_dict, *opt):
+        " This is open for the implementation of a user-defined simulator class"
+        dummy = 1
+        return cls(dummy)
+
+    @clasmethod
+    def application(cls, param_dict, *opt):
+        " This is open for the implementation of connection to a real plant"
+        dummy = 1
+        return cls(dummy)
+
+    def simulator_step(simulator, args):
+        x = observer(args)
+        return x
+
+class optimizer:
+    '''This is a class that defines a do-mpc optimizer. The class uses a local model, which
+    can be defined independetly from the other modules. The parameters '''
+    def __init__(self, optimizer_model, param_dict, *opt):
+        # Set the local model to be used by the model
+        self.optimizer_model = optimizer_model
+        # Assert for the required size of the parameters
+        required_dimension = 14
+        try:
+             assert(len(param_dict)==required_dimension)
+        raise Exception("The length of the parameter dictionary is not correct!")
+        # Define optimizer parameters
+        self.n_horizon = param_dict["n_horizon"]
+        self.t_step = param_dict["t_step"]
+        self.n_robust = param_dict["n_robust"]
+        self.poly_degree = param_dict["poly_degree"]
+        self.collocation = param_dict["collocation"]
+        self.n_fin_elem = param_dict["n_fin_elem"]
+        self.generate_code = param_dict["generate_code"]
+        self.open_loop = param_dict["open_loop"]
+        self.t_end = param_dict["t_end"]
+        self.nlp_solver = param_dict["nlp_solver"]
+        self.linear_solver = param_dict["linear_solver"]
+        self.qp_solver = param_dict["qp_solver"]
+        # Define model uncertain parameters
+        self.uncertainty_values = param_dict["uncertainty_values"]
+        # Defin time varying optimizer parameters
+        self.parameters_NLP = param_dict["parameters_NLP"]
+    @classmethod
+    def user_optimizer(cls, optimizer_model, param_dict, *opt):
+        "This method is open for the impelmentation of a user defined optimizer"
+        dummy = 1
+        return cls(dummy)
+    def optimizer_step(solver, args):
+        result = solver(x0=arg['x0'], lbx=arg['lbx'], ubx=arg['ubx'], lbg=arg['lbg'], ubg=arg['ubg'])
+        return result
+
+class observer:
+    """A class for the definition model equations and optimal control problem formulation"""
+    def __init__(self, model_observer, param_dict, *opt):
+        self.x = param_dict['x']
+        self.observer = param_dict['observer']
+    def observer_step(observer, args):
+        x = observer(args) # TODO: this should probably be updated
+        return x
+
+    @clasmethod
+    def user_observer(cls, param_dict, *opt):
+        " This is open for the implementation of a user-defined estimator class"
+        dummy = 1
+        return cls(dummy)
