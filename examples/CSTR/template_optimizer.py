870b58273c8e1237c88359766fd427a6a2360503
sergiolucia <sergiolucia15@gmail.com>
2016-06-04 23:05:23 +0200

    all linear solvers set by default to mumps to avoid errors of users without hsl solvers

--- examples/CSTR/template_optimizer.py
+++ examples/CSTR/template_optimizer.py
@@ -59,7 +59,7 @@ def optimizer(model):
     # such as the hsl linear solver MA27, which can be downloaded as a precompiled
     # library and can be used by IPOPT on run time
 
-    linear_solver = 'ma27'
+    linear_solver = 'mumps'
 
     # GENERATE C CODE shared libraries NOTE: Not currently supported
     generate_code = 0
