bb4c9d1d37b20250de39bcf4d9698e77533a3a87
Sergio Lucia <sergio.lucia@tu-berlin.de>
2018-08-07 16:28:29 +0200

    updated copyright notice for the inverted pendulum example

--- examples/inverted_pendulum/template_model.py
+++ examples/inverted_pendulum/template_model.py
@@ -1,32 +1,24 @@
-# 	 -*- coding: utf-8 -*-
 #
-#    This file is part of DO-MPC
+#   This file is part of do-mpc
 #
-#    DO-MPC: An environment for the easy, modular and efficient implementation of
-#            robust nonlinear model predictive control
+#   do-mpc: An environment for the easy, modular and efficient implementation of
+#        robust nonlinear model predictive control
 #
-#    The MIT License (MIT)
+#   Copyright (c) 2014-2018 Sergio Lucia, Alexandru Tatulea-Codrean
+#                        TU Dortmund. All rights reserved
 #
-#    Copyright (c) 2014-2018 Sergio Lucia, Alexandru Tatulea-Codrean
-#                            TU Dortmund. All rights reserved
+#   do-mpc is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as
+#   published by the Free Software Foundation, either version 3
+#   of the License, or (at your option) any later version.
 #
-#    Permission is hereby granted, free of charge, to any person obtaining a copy
-#    of this software and associated documentation files (the "Software"), to deal
-#    in the Software without restriction, including without limitation the rights
-#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#    copies of the Software, and to permit persons to whom the Software is
-#    furnished to do so, subject to the following conditions:
+#   do-mpc is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
 #
-#    The above copyright notice and this permission notice shall be included in all
-#    copies or substantial portions of the Software.
-#
-#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#    SOFTWARE.
+#   You should have received a copy of the GNU General Public License
+#   along with do-mpc.  If not, see <http://www.gnu.org/licenses/>.
 #
 from casadi import *
 import numpy as NP
@@ -44,7 +36,7 @@ def model():
     l   = 1.0                          # length of lever arm            [m]
     h   = 0.5                          # height of rod connection point [m]
     g   = 9.81                         # grav acceleration              [m/s^2]
-    
+
     """
     --------------------------------------------------------------------------
     template_model: define uncertain parameters, states and controls as symbols
@@ -52,20 +44,20 @@ def model():
     """
     # Define the uncertain/varying parameters as CasADi symbols
     alpha      = SX.sym("alpha")         # can be used for implementing uncertainty
-    
+
     tv_param_1 = SX.sym("tv_param_1")    # can be used to implement setpoint change
-    
+
     # Define the differential states as CasADi symbols
     x 	   = SX.sym("x")               # x position of the mass
     y 	   = SX.sym("y")		          # y position of the mass
     v     = SX.sym("v")              # linear velocity of the mass
     theta = SX.sym("theta")          # angle of the metal rod
     omega = SX.sym("omega")          # angular velocity of the mass
-    # Define the algebraic states as CasADi symbols  
+    # Define the algebraic states as CasADi symbols
     #y 		= SX.sym("y")          # the y coordinate is considered algebraic
-    
+
     # Define the control inputs as CasADi symbols
-    F  	= SX.sym("F")             # control force applied to the lever 
+    F  	= SX.sym("F")             # control force applied to the lever
 
     """
     --------------------------------------------------------------------------
@@ -78,7 +70,7 @@ def model():
     dd[1]  = (1.0/(M+m-m*casadi.cos(theta)))*(m*g*casadi.sin(theta)-m*l*casadi.sin(theta)*omega**2 +F)
     dd[2] = omega
     dd[3] = (1.0/l)*(dd[1]*casadi.cos(theta)+g*casadi.sin(theta))
-   
+
     # Define the algebraic equations
     dy = h+l*casadi.cos(theta) - y            # the coordinates must fulfil the constraint of the lever arm length
 
@@ -97,7 +89,7 @@ def model():
 
     _zdot = []                          # toggle if there are no AE in your model
     #_zdot = vertcat(dtrajectory)
-    
+
     _tv_p = vertcat(tv_param_1)
     """
     --------------------------------------------------------------------------
@@ -109,9 +101,9 @@ def model():
     v_init  = 0.0
     t_init  = 1.5
     o_init  = 0.0
-    
+
     #Initial conditions for Algebraic States
-    y_init  = sqrt(3.0)/2.0 
+    y_init  = sqrt(3.0)/2.0
 
     x0 = NP.array([x_init, v_init, t_init, o_init])
     z0 = NP.array([])
@@ -120,7 +112,7 @@ def model():
     v_lb    = -10.0;       v_ub  = 10.0
     t_lb    =   -100*pi;       t_ub  = 100*pi
     o_lb    = -10.0;       o_ub  = 10.0
-   
+
 
     x_lb = NP.array([x_lb, v_lb, t_lb, o_lb])
     x_ub = NP.array([x_ub, v_ub, t_ub, o_ub])
@@ -128,7 +120,7 @@ def model():
     z_ub = NP.array([])
     # Bounds on the control inputs. Use "inf" for unconstrained inputs
     F_lb    = -250.0;       F_ub = 250.00 ;     F_init = 0.0	;
-   
+
 
     u_lb=NP.array([F_lb])
     u_ub=NP.array([F_ub])
@@ -186,4 +178,3 @@ def model():
     model = core_do_mpc.model(model_dict)
 
     return model
-
