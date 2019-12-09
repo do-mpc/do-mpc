5ae888ddb37f4c835fb8dc3957dc292dcf315c9c
ffiedler <felix.mh.fiedler@gmail.com>
2019-12-09 15:03:58 +0100

    Doc strings

--- new_version/do_mpc/backend_graphics.py
+++ new_version/do_mpc/backend_graphics.py
@@ -22,27 +22,72 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.axes as maxes
 from casadi import *
 from casadi.tools import *
 import pdb
 
 
 class backend_graphics:
+    """Graphics module to present the results of do-mpc. The module is independent of all other modules but is incorporated in the configuration.
+    This allows for a high-level API which is triggered through the configuration.
+    At the same time, the module can be used with pickled result files in post-processing for flexible and custom graphics.
+    For details about the high-level API, please see the doc string in configuration.setup_graphic().
+    Using the low-level API consists of two steps.
+    First, the .add_line method is used to define which results are to be plotted. The user passes an existing axes object.
+    Note that .add_line does not create a graphic.
+    The graphic is obtained with the .plot_results() method, which takes a do-mpc data object as input. Each module (simulator, estimator, optimizer)
+    has its own data object.
+    Furthermore, the module contains the .plot_predictions() method, which can be used to show the predicted trajectories.
+    """
     def __init__(self):
         self.line_list = []
         self.ax_list  = []
         self.color = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
     def reset_axes(self):
+        """Clears the lines on all axes which were passed with .add_line.
+        Method is called internally, before each plot.
+        """
         for ax_i in self.ax_list:
             ax_i.lines = []
         self.reset_prop_cycle()
 
     def reset_prop_cycle(self):
+        """Resets the property cycle for all axes which were passed with .add_line.
+        The matplotlib color cycler is restarted.
+        """
         for ax_i in self.ax_list:
             ax_i.set_prop_cycle(None)
 
-    def add_line(self, var_type, var_name, axis , **pltkwargs):
+    def add_line(self, var_type, var_name, axis, **pltkwargs):
+        """add_line is called during setting up the graphics class. This is typically the last step of the configuration of a do-mpc case.
+        Each call of .add_line adds a line to the passed axis according to the variable type (_x, _u, _z, _tvp, _p, _aux_expression)
+        and its name (as defined in the model).
+        Furthermore, all valid matplotlib .plot arguments can be passed as optional keyword arguments, e.g.: 'linewidth', 'color', 'alpha'.
+
+        :param var_type: Variable type to be plotted. Valid arguments are _x, _u, _z, _tvp, _p, _aux_expression.
+        :type var_type: string
+
+        :param var_name: Variable name. Must reference the names defined in the model for the given variable type.
+        :type var_name: string
+
+        :param axis: Variable name. Must reference the names defined in the model for the given variable type.
+        :type axis: matplotlib.axes.Axes object.
+
+        :param pltkwargs: Valid matplotlib pyplot keyword arguments (e.g.: 'linewidth', 'color', 'alpha')
+        :type pltkwargs: (, optional)
+
+        :raises assertion: var_type argument must be a string
+        :raises assertion: var_name argument must be a string
+        :raises assertion: var_type argument must reference to the valid var_types of do-mpc models.
+        :raises assertion: axis argument must be matplotlib axes object.
+        """
+        assert isinstance(var_type, str), 'var_type argument must be a string. You have: {}'.format(type(var_type))
+        assert isinstance(var_name, str), 'var_name argument must be a string. You have: {}'.format(type(var_name))
+        assert var_type in ['_x', '_u', '_z', '_tvp', '_p'], 'var_type argument must reference to the valid var_types of do-mpc models.'
+        assert isinstance(axis, maxes.Axes), 'axis argument must be matplotlib axes object.'
+
         self.line_list.append(
             {'var_type': var_type, 'var_name': var_name, 'ax': axis, 'reskwargs': pltkwargs, 'predkwargs':pltkwargs.copy()}
         )
@@ -50,11 +95,34 @@ class backend_graphics:
 
 
     def plot_results(self, data, t_ind=None, **pltkwargs):
-        self.reset_prop_cycle()
+        """Plots the results stored in the passed data object for the plot configuration.
+        Each do-mpc module (simulator, estimator, optimizer) has an individual data object.
+        Use the t_ind parameter to plot only until the given time index. This is mostly used in post-processing for animations.
+        Optionally pass an arbitrary number of valid pyplot.plot arguments (e.g. 'color', 'linewidth', 'alpha'), which is applied to ALL lines.
+
+        :param data: do-mpc data object. Either from unpickled results or the created modules. The data object is updated at each make_step_... call in the configuration.
+        :type data: do-mpc data object
+
+        :param t_ind: Plot results up until this time index.
+        :type t_ind: int
+
+        :param pltkwargs: Valid matplotlib pyplot keyword arguments (e.g.: 'linewidth', 'color', 'alpha')
+        :type pltkwargs: (, optional)
+
+        :raises assertion: t_ind argument must be a int
+        :raises assertion: t_ind argument must not exceed the length of the results
+
+        :return: All plotted lines on all supplied axes.
+        :rtype:  list
+        """
+        if t_ind is not None:
+            assert isinstance(t_ind, int), 'The t_ind param must be of type int. You have: {}'.format(type(t_ind))
+            assert t_ind <= data._time.shape[0], 'The t_ind param must not exceed the length of the results. You choose t_ind={}, where only n={} elements are available.'.format(t_ind, data._time.shape[0])
         # Make index "inclusive", if it is passed. This means that for index 1, the elements at 0 AND 1 are plotted.
         if t_ind is not None:
             t_ind+=1
 
+        self.reset_prop_cycle()
         lines = []
         for line_i in self.line_list:
             line_i['reskwargs'].update(pltkwargs)
@@ -70,7 +138,30 @@ class backend_graphics:
         return lines
 
     def plot_predictions(self, data, opt_x_num=None, t_ind=-1, **pltkwargs):
+        """Plots the predicted trajectories for the plot configuration.
+        The predicted trajectories are part of the optimal solution at each timestep and can be passed either as the optional
+        argument (opt_x_num) or they are part of the data structure, if the optimizer was set to store the optimal solution.
+        The plot predictions method can only be called with data from the do-mpc optimizer object and raises an error if called with data from other objects.
+        Use the t_ind parameter to plot the prediction for the given time instance. This is mostly used in post-processing for animations.
+        Optionally pass an arbitrary number of valid pyplot.plot arguments (e.g. 'color', 'linewidth', 'alpha'), which is applied to ALL lines.
+
+        :param data: do-mpc (optimizer) data object. Either from unpickled results or the created modules.
+        :type data: do-mpc (optimizer) data object
+
+        :param t_ind: Plot predictions at this time index.
+        :type t_ind: int
+
+        :param pltkwargs: Valid matplotlib pyplot keyword arguments (e.g.: 'linewidth', 'color', 'alpha')
+        :type pltkwargs: (, optional)
+
+        :raises assertion: Can only call plot_predictions with data object from do-mpc optimizer
+        :raises Exception: Cannot plot predictions if full solution is not stored or supplied when calling the method
+
+        :return: All plotted lines on all supplied axes.
+        :rtype:  list
+        """
         assert data.dtype == 'optimizer', 'Can only call plot_predictions with data object from do-mpc optimizer.'
+        assert isinstance(t_ind, int), 'The t_ind param must be of type int. You have: {}'.format(type(t_ind))
 
         t_now = data._time[t_ind]
         # These fields only exist, if data type (dtype) os optimizer:
@@ -116,3 +207,5 @@ class backend_graphics:
                 pred = vertcat(*opt_x_num[line_i['var_type'],:,lambda v: horzcat(*v),:,line_i['var_name']])
                 pred = pred.full()[range(pred.shape[0]),structure_scenario[:-1,:].T].T
                 lines.extend(line_i['ax'].step(time, pred, **line_i['predkwargs']))
+
+        return lines
