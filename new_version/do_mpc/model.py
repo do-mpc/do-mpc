4dc434504fbffeaf31916ece96fb06caf8cdb630
ffiedler <felix.mh.fiedler@gmail.com>
2020-01-06 18:20:35 +0100

    Added measurement function (+ Docu for Model)

--- new_version/do_mpc/model.py
+++ new_version/do_mpc/model.py
@@ -27,40 +27,51 @@ import pdb
 
 
 class model:
-    """The do_mpc model class. This class holds the full model description and is at the core of
-    simulator, optimizer and estimator. The model class is created with setting the model_type (continuous or discrete).
-    The final model is created with the method :py:func:`model.setup_model`.
-    Prior to that call the model variables have to be declared (with :py:func:`model.set_variable`)
-    as well as auxiliary expressions with :py:func:`model.set_expression` (optional)
-    and finally the right hand side (rhs) of the ODE with :py:func:`model.set_rhs`.
+    """The "**do mpc**" model class. This class holds the full model description and is at the core of
+    :py:class:`do_mpc.simulator`, :py:class:`do_mpc.optimizer` and :py:class:`do_mpc.estimator`.
+    The :py:class:`do_mpc.model` class is created with setting the `model_type` (continuous or discrete).
+    A ``continous`` model consists of an underlying ordinary differential equation (ODE) or differential algebraic equation (DAE):
+
+    .. math::
+
+       \dot{x} = f(x,u,z,p_{tv},p)
+
+       0 = g(x,u,z,p_{tv},p)
+
+       y = h(x,u,z)
+
+    whereas a ``discrete`` model consists of a difference equation:
+
+    .. math::
+
+       x_{k+1} = f(x_{k},u_{k},z_{k},p_{tv,k},p)
+
+       0 = g(x_{k},u_{k}, z_{k},p_{tv,k},p)
+
+       y_k = h(x_k,u_k,z_k)
+
+
+    **Configuration and setup:**
+
+    Configuring and setting up the optimizer involves the following steps:
+
+    1. Use :py:func:`model.set_variable` to introduce new variables to the model. It is important to declare the variable type (states, inputs, etc.).
+
+    2. Optionally introduce "auxiliary" expressions as functions of the previously defined variables with :py:func:`model.set_expression`. The expressions can be used for monitoring or be reused as constraints, the cost function etc.
+
+    3. Optionally introduce measurement equations with :py:func:`model.set_meas`. The syntax is identical to :py:func:`model.set_expression`. By default state-feedback is assumed.
+
+    4. Define the right-hand-side of the `discrete` or `continuous` model as a function of the previously defined variables with :py:func:`model.set_rhs`. This method must be called once for each introduced state.
+
+    5. Call :py:func:`model.setup_model` to finalize the ``model``. No further changes are possible afterwards.
 
     :param model_type: Set if the model is ``discrete`` or ``continuous``.
     :type var_type: str
 
+    :raises assertion: model_type must be string
+    :raises assertion: model_type must be either discrete or continuous
     """
     def __init__(self, model_type=None):
-        """Initiate the do_mpc model class. This class holds the full model description and is at the core of
-        simulator, optimizer and estimator. The model class is created with setting the model_type (continuous or discrete).
-        The final model is created with the method .setup_model().
-        Prior to that call the following variables have to be declared (with .set_variable):
-        - states (_x)
-        - inputs (_u)
-        - algebraic (_z)
-        - parameter (_p)
-        - timevarying_parameter (_tvp)
-        As well as auxiliary expressions (with .set_expression)
-        and finally the right hand side (rhs) of the ODE with .set_rhs().
-
-        :param model_type: Set if the model is discrete or continuous
-        :type var_type: string
-
-        :raises assertion: var_type must be string
-        :raises assertion: var_name must be string
-        :raises assertion: shape must be tuple or int
-
-        :return: None
-        :rtype: None
-        """
         assert isinstance(model_type, str), 'model_type must be string, you have: {}'.format(type(model_type))
         assert model_type in ['discrete', 'continuous'], 'model_type must be either discrete or continuous, you have: {}'.format(model_type)
 
@@ -78,6 +89,7 @@ class model:
             {'expr_name': 'default', 'expr': DM()}
         ]
         self.rhs_list = []
+        self.meas_list = []
 
         self.flags = {
             'setup': False
@@ -171,6 +183,29 @@ class model:
 
         return expr
 
+    def set_meas(meas_name, expr):
+        """Introduce new measurable output to the model class. By default, the model assumes state-feedback (all states are measured outputs).
+        Expressions must be formulated with respect to ``_x``, ``_u``, ``_z``, ``_tvp``, ``_p``.
+
+        :param expr_name: Arbitrary name for the given expression. Names are used for key word indexing.
+        :type expr_name: string
+        :param expr: CasADi SX or MX function depending on ``_x``, ``_u``, ``_z``, ``_tvp``, ``_p``.
+        :type expr: CasADi SX or MX
+
+        :raises assertion: expr_name must be str
+        :raises assertion: expr must be a casadi SX or MX type
+        :raises assertion: Cannot call after .setup_model.
+
+        :return: Returns the newly created measurement expression.
+        :rtype: casadi.SX
+        """
+        assert self.flags['setup'] == False, 'Cannot call .set_meas after .setup_model.'
+        assert isinstance(meas_name, str), 'meas_name must be str, you have: {}'.format(type(meas_name))
+        assert isinstance(expr, (casadi.SX, casadi.MX)), 'expr must be a casadi SX or MX type, you have:{}'.format(type(expr))
+        self.meas_list.extend([{'meas_name': meas_name, 'expr': expr}])
+
+        return expr
+
     def set_rhs(self, var_name, expr):
         """Formulate the right hand side (rhs) of the ODE. Each defined state variable must have a respective equation (of matching dimension)
         for the rhs. Match the rhs with the state by choosing the corresponding names.
@@ -208,8 +243,28 @@ class model:
         self.rhs_list.extend([{'var_name': var_name, 'expr': expr}])
 
     def get_variables(self):
-        # TODO: Add docstring.
+        """Method to retrieve the variables of the model. This method is convenient when creating the model in a different file
+        than the, e.g. the :py:class:`do_mpc.optimizer`. Returns the variables as a list with the following order:
 
+        * ``_x`` (states)
+
+        * ``_u`` (inputs)
+
+        * ``_z`` (algebraic variables)
+
+        * ``_tvp`` (time varying parameters)
+
+        * ``_p`` (uncertain parameters)
+
+        * ``_aux`` (auxiliary expressions)
+
+        The method cannot be called prior to :py:func:`model.setup`.
+
+        :raises assertion: Model was not setup. Finish model creation by calling model.setup_model().
+
+        :return: List of model variables (``_x``, ``_u``, ``_z``, ``_tvp``, ``_p``, ``_aux``)
+        :rtype: list
+        """
         assert self.flags['setup'] == True, 'Model was not setup. Finish model creation by calling model.setup_model().'
 
         return self._x, self._u, self._z, self._tvp, self._p, self._aux_expression
@@ -242,7 +297,18 @@ class model:
             entry(expr_i['expr_name'], expr=expr_i['expr']) for expr_i in self.expr_list
         ])
 
-
+        # Write self._y (measurement equations) as struct_SX symbolic expression structures.
+        # Use the previously defined SX.sym variables to declare shape and symbolic variable.
+        # If no measurement expressions are declared, use state-feedback as default measurement.
+        if not self.meas_list:
+            self._y = struct_SX([
+                entry(var_k['var_name'], expr=var_k['value']) for var_k in self.var_list['_x']
+            ])
+        else:
+            self._y = struct_SX([
+                entry(meas_i['meas_name'], expr=meas_i['expr']) for meas_i in self.meas_list
+            ])
+        assert set(symvar(self._y)).issubset(set(symvar(self._x))), 'Measurement equation must be solely depending on states (_x).'
 
         # Create mutable struct_SX with identical structure as self._x to hold the right hand side.
         self._rhs = struct_SX(self._x)
@@ -258,11 +324,13 @@ class model:
         # Declare functions for the right hand side and the aux_expressions.
         _x, _u, _z, _tvp, _p, _aux = self.get_variables()
         self._rhs_fun = Function('rhs_fun', [_x, _u, _z, _tvp, _p], [self._rhs])
-        self._aux_expression_fun = Function('aux_expression_fun', [_x, _u, _z, _tvp, _p], [_aux])
+        self._aux_expression_fun = Function('aux_expression_fun', [_x, _u, _z, _tvp, _p], [self._aux_expression])
+        self._meas_fun = Function('meas_fun', [_x, _u, _z, _tvp, _p]], [self._y])
 
         # Create and store some information about the model regarding number of variables for
-        # x, _u, _z, _tvp, _p, _aux
+        # _x, _y, _u, _z, _tvp, _p, _aux
         self.n_x = self._x.shape[0]
+        self.n_y = self._y.shape[0]
         self.n_u = self._u.shape[0]
         self.n_z = self._z.shape[0]
         self.n_tvp = self._tvp.shape[0]
@@ -271,5 +339,6 @@ class model:
 
         # Remove temporary storage for the symbolic variables. This allows to pickle the class.
         delattr(self, 'var_list')
+        delattr(self, 'meas_list')
         delattr(self, 'expr_list')
         delattr(self, 'rhs_list')
