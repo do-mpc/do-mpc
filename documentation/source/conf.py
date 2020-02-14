c3ae932c1adc7f353a0b59c981e78e94771929c9
ffiedler <felix.mh.fiedler@gmail.com>
2020-02-14 15:54:25 +0100

    Changed documentation folder name

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -31,7 +31,7 @@ release = '4.0.0'
 # -- General configuration ---------------------------------------------------
 
 # The master toctree document.
-master_doc = 'contents'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
