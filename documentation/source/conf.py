d305f207744dc3426ed92f2ec0df9a6de68bc58d
ffiedler <felix.mh.fiedler@gmail.com>
2020-02-14 16:34:47 +0100

    Updated gitignore // added gifs

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -16,7 +16,7 @@
 import pdb
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
