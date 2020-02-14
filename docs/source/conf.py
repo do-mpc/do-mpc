f64c369aa0b028ec4a4dac99ad70c6fc11750f15
ffiedler <felix.mh.fiedler@gmail.com>
2020-02-14 14:53:00 +0100

    rtd fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ release = '4.0.0'
 
 
 # -- General configuration ---------------------------------------------------
-master_doc = 'overview'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
