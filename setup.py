48e8ac2a1c68fe2976e6c018c61cdf66a2e840c9
ffiedler <felix.mh.fiedler@gmail.com>
2020-02-14 17:32:46 +0100

    Working on pip installation

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     packages=['do_mpc','do_mpc.tools'],
     author='Sergio Lucia',
     author_email='sergio.lucia@tu-berlin.de',
+    url='https://do-mpc.readthedocs.io',
     license='LICENSE.txt',
     long_description=open('README.md', 'r').read(),
     long_description_content_type="text/markdown",
     install_requires=[
-        "casadi >= 3.5.1",
-        "numpy >= 1.17.2",
-        "jupyter >= 1.0.0",
-        "matplotlib >= 3.1.1"
+        "casadi",
+        "numpy",
+        "matplotlib"
     ],
 )
