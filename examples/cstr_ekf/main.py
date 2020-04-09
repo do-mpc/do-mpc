4147ac606ab0f45469f99612e8c02db1b6cf14cf
Alexandru <tatulea.alex@gmail.com>
2020-04-09 17:45:06 +0200

    Updated the EKF example with new graphics after merging master. Tested w/o parameter estimation and w/o output feednack.

--- examples/cstr_ekf/main.py
+++ examples/cstr_ekf/main.py
@@ -38,11 +38,15 @@ from template_mpc import template_mpc
 from template_simulator import template_simulator
 from template_ekf import template_ekf
 
+""" User settings: """
+output_feedback = False     # Do not use the estimator in the MPC loop (only use EKF data for comparison)
+show_animation  = True      # Display live animation at runtime
+store_results   = False     # Do not store pickled results (if True, creates a new result folder and stores each run under different name)
 
 model = template_model()
+sim = template_simulator(model)
 mpc = template_mpc(model)
-simulator = template_simulator(model)
-estimator = template_ekf(model)
+ekf = template_ekf(model)
 
 # Set the initial state of mpc and simulator:
 C_a_0   = 0.8       # This is the initial concentration inside the tank [mol/l]
@@ -55,29 +59,45 @@ beta_0  = 0.95      # The reaction rate coeff of B-->C to be estimated (nominal 
 x0 = np.array([C_a_0, C_b_0, T_R_0, T_K_0]).reshape(-1,1)
 
 mpc.set_initial_state(x0, reset_history=True)
-simulator.set_initial_state(x0, reset_history=True)
+sim.set_initial_state(x0, reset_history=True)
 
 "Estimator initialization: select 1) only state estimation or 2) state and parameter estimation"
+"(!): Remember to change the EKF tuning accordingly in template_ekf"
 #x0_hat = np.array([0.8, 0.5, 100.0, 100.0]).reshape(-1,1)          # 1) only state estimation
 x0_hat = np.array([0.8, 0.5, 100.0, 100.0, alpha_0]).reshape(-1,1)  # 2) state and parameter estimation
-estimator.set_initial_state(x0_hat, reset_history=True)
+ekf.set_initial_state(x0_hat, reset_history=True)
 
 # Initialize graphic:
-graphics = do_mpc.graphics.Graphics()
+mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
+ekf_graphics = do_mpc.graphics.Graphics(ekf.data)
 
-
-fig, ax = plt.subplots(4, sharex=True)
+fig, ax = plt.subplots(5, sharex=True)
 # Configure plot:
-graphics.add_line(var_type='_x', var_name='C_a', axis=ax[0])
-graphics.add_line(var_type='_x', var_name='C_b', axis=ax[0])
-graphics.add_line(var_type='_x', var_name='T_R', axis=ax[1])
-graphics.add_line(var_type='_x', var_name='T_K', axis=ax[1])
-graphics.add_line(var_type='_u', var_name='Q_dot', axis=ax[2])
-graphics.add_line(var_type='_u', var_name='F', axis=ax[3])
+# 1) Create the MPC lines
+mpc_graphics.add_line(var_type='_x', var_name='C_a', axis=ax[0])
+mpc_graphics.add_line(var_type='_x', var_name='C_b', axis=ax[0])
+mpc_graphics.add_line(var_type='_x', var_name='T_R', axis=ax[1])
+mpc_graphics.add_line(var_type='_x', var_name='T_K', axis=ax[1])
+mpc_graphics.add_line(var_type='_aux', var_name='T_dif', axis=ax[2])
+mpc_graphics.add_line(var_type='_u', var_name='Q_dot', axis=ax[3])
+mpc_graphics.add_line(var_type='_u', var_name='F', axis=ax[4])
+# 2) Add the estimated lines to be compared to the MPC results 
+#   (if output_feedback = False, the lines are only for comparison purposes)
+ekf_graphics.add_line(var_type='_x', var_name='C_a', axis=ax[0], linestyle = '-.', color='#1f77b4')
+ekf_graphics.add_line(var_type='_x', var_name='C_b', axis=ax[0], linestyle = '-.', color='#ff7f0e')
+
 ax[0].set_ylabel('c [mol/l]')
-ax[1].set_ylabel('Temperature [K]')
-ax[2].set_ylabel('Q_heat [kW]')
-ax[3].set_ylabel('Flow [l/h]')
+ax[1].set_ylabel('T [K]')
+ax[2].set_ylabel('$\Delta$ T [K]')
+ax[3].set_ylabel('Q [kW]')
+ax[4].set_ylabel('Flow [l/h]')
+ax[4].set_xlabel('Time [h]')
+
+label_lines = mpc_graphics.result_lines['_x', 'C_a']+mpc_graphics.result_lines['_x', 'C_b'] + \
+              ekf_graphics.result_lines['_x', 'C_a']+ekf_graphics.result_lines['_x', 'C_b']
+ax[0].legend(label_lines, ['True $C_a$', 'True $C_b$', 'Estimated $C_a$', 'Estimated $C_b$'])
+label_lines = mpc_graphics.result_lines['_x', 'T_R']+mpc_graphics.result_lines['_x', 'T_K']
+ax[1].legend(label_lines, ['$T_R$', '$T_K$'])
 
 fig.align_ylabels()
 plt.ion()
@@ -86,36 +106,43 @@ time_list = []
 for k in range(100):
     
     if k==0: xk = x0; xk_hat = x0
-    "Note: This is an output-feedback NMPC scheme. If state-feedback is desired, use `xk` in the mpc call"
-    
+    "Note: This is an output-feedback NMPC scheme. If state-feedback is desired, update the setting at the top of this script"   
     tic = time.time()
     "Step 1: Call the NMPC controller and obtain the new optimal inputs `uk`"
     uk = mpc.make_step(xk)
     "Step 2: Call the simulator. A predefined output vector is returned `yk`"
-    yk = simulator.make_step(uk)
-    xk = simulator._x0.cat
+    yk = sim.make_step(uk)
     "Note: The EKF needs both the inputs and the current parameter values"
-    pk = simulator.p_fun(simulator._t0).cat.toarray()
+    pk = sim.p_fun(sim._t0).cat.toarray()
     "Step 3: Call the EKF with previously obtained data at step `k`"
-    xk_hat, pk_hat = estimator.make_step(yk, uk, pk)
-    #pdb.set_trace()
+    xk_hat, pk_hat = ekf.make_step(yk, uk, pk)
     "Note: If parameter estimation was selected, pk_hat will be non-empty. Otherwise pk_hat=[]"
+    if output_feedback : 
+        # The full state of the plant will be fed into the MPC
+        xk = xk_hat
+    else:
+        #The estimated state will be used
+        xk = sim._x0.cat
+    
     toc = time.time()
     time_list.append(toc-tic)
+    "The live animations can be activated at the top of the script by setting show_animation=True"
+    if show_animation:
+        mpc_graphics.plot_results(t_ind=k)
+        ekf_graphics.plot_results(t_ind=k)
+        mpc_graphics.plot_predictions(t_ind=k)
+        mpc_graphics.reset_axes()
+        plt.show()
+        plt.pause(0.01)
 
 time_arr = np.array(time_list)
 print('Total run-time: {tot:5.2f} s, step-time {mean:.3f}+-{std:.3f} s.'.format(tot=np.sum(time_arr), mean=np.mean(time_arr), std=np.sqrt(np.var(time_arr))))
 
-est_lines = graphics.plot_results(estimator.data)#, color='black', linestyle='-.')
-sim_lines = graphics.plot_results(simulator.data)
+est_lines = ekf_graphics.plot_results()
+sim_lines = mpc_graphics.plot_results()
 
-
-plt.sca(ax[0])
-ax[0].add_artist(plt.legend(sim_lines[:2], ['Ca', 'Cb'], title='Estimator', loc=1))
-plt.sca(ax[0])
-ax[0].add_artist(plt.legend(est_lines[:2], ['Ca', 'Cb'], title='Simulator', loc=2))
-plt.show()
 input('Press any key to exit.')
 
 # Store results:
-#do_mpc.data.save_results([mpc, simulator, estimator], 'cstr_MPC_EKF')
+if store_results:
+    do_mpc.data.save_results([mpc, simulator, estimator], 'cstr_NMPC_EKF')
