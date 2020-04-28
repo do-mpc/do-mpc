7d2289935e9bd975484a604b15b1903117b6228c
4flixt <32864011+4flixt@users.noreply.github.com>
2020-04-28 16:52:32 +0200

    Update setup.py
    New website URL and changed version name.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='do_mpc',
-    version='4.0.0-beta1',
+    version='4.0.0-beta2',
     packages=['do_mpc','do_mpc.tools'],
     author='Sergio Lucia and Felix Fiedler',
     author_email='sergio.lucia@tu-berlin.de',
-    url='https://do-mpc.readthedocs.io',
+    url='www.do-mpc.com',
     license='GNU LESSER GENERAL PUBLIC LICENSE',
     long_description=open('README.md', 'r').read(),
     long_description_content_type="text/markdown",
