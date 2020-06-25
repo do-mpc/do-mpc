7127a7bf343a49c774af2f5748067a9fcf556c94
Felix-Mac <felix.fiedler@tu-berlin.de>
2020-06-25 16:38:46 +0200

    Version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='do_mpc',
-    version='4.0.0-beta3',
+    version='4.0.0',
     packages=['do_mpc','do_mpc.tools'],
     author='Sergio Lucia and Felix Fiedler',
     author_email='sergio.lucia@tu-berlin.de',
