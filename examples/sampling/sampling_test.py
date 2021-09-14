49340668c91257feed5dfdf645490b3e9822527f
Felix-Mac <felix.fiedler@tu-berlin.de>
2021-09-14 15:42:08 +0200

    Added DataHandler to sampling_test

--- examples/sampling/sampling_test.py
+++ examples/sampling/sampling_test.py
@@ -5,18 +5,22 @@ import do_mpc
 import numpy as np
 import pdb
 
+np.random.seed(123)
 
 sp = do_mpc.sampling.SamplingPlanner()
+sp.set_param(overwrite = True)
 
 
 sp.set_sampling_var('alpha', np.random.randn)
 sp.set_sampling_var('beta', lambda: np.random.randint(0,5))
 
+sp.set_param(save_format='pickle')
+
 plan = sp.gen_sampling_plan('test', n_samples=10)
 
 
 sampler = do_mpc.sampling.Sampler(plan)
-sampler.set_param(save_format='mat')
+
 
 def sample_function(alpha, beta):
     return alpha*beta
@@ -24,3 +28,20 @@ def sample_function(alpha, beta):
 sampler.set_sample_function(sample_function)
 
 sampler.sample_data()
+
+
+
+
+dh = do_mpc.sampling.DataHandler(plan)
+
+dh.set_compilation_function(lambda x: x)
+
+
+res = dh[lambda: True]
+
+pdb.set_trace()
+#
+# comp.set_compilation_function()
+#
+#
+# comp[alpha<3, beta=2]
