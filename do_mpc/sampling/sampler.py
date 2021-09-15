703049f1ca12a07286706c4e7bc2c5c3a1c70d99
Felix-Mac <felix.fiedler@tu-berlin.de>
2021-09-15 11:46:16 +0200

    Added annotations and minor changes to Sampler class

--- do_mpc/sampling/sampler.py
+++ do_mpc/sampling/sampler.py
@@ -1,12 +1,13 @@
 import types
 import pickle
 import os
+import inspect
 import numpy as np
 import pathlib
 import pdb
 import scipy.io as sio
 import copy
-from do_mpc.tools import load_pickle, save_pickle
+from do_mpc.tools import load_pickle, save_pickle, printProgressBar
 
 
 class Sampler:
@@ -47,7 +48,18 @@ class Sampler:
         pathlib.Path(val).mkdir(parents=True, exist_ok=True)
 
     def set_param(self, **kwargs):
-        """
+        """Configure the :py:class:Sampler`` class.
+
+        Parameters must be passed as pairs of valid keywords and respective argument.
+        For example:
+
+        ::
+
+            sampler.set_param(overwrite = True)
+
+
+        :param overwrite: Should previously created results be overwritten. Default is ``False`
+        :type overwrite: bool
 
         """
         for key, value in kwargs.items():
@@ -60,9 +72,10 @@ class Sampler:
     def set_sample_function(self, sample_function):
         """
         Set sample generating function.
-        The sampling function produces a sample result for each sample definition in the ``sampling_plan``.
+        The sampling function produces a sample result for each sample definition in the ``sampling_plan``
+        and is called in the method :py:meth:`sample_data`.
 
-        It is important that the sample function only uses keyword arguments with the same name as previously defined in the ``sampling_plan``.
+        It is important that the sample function only uses keyword arguments **with the same name as previously defined** in the ``sampling_plan``.
 
         **Example:**
 
@@ -75,7 +88,7 @@ class Sampler:
 
             sampler = do_mpc.sampling.Sampler(plan)
 
-            def sample_function(alpha, beta, gamma):
+            def sample_function(alpha, beta):
                 return alpha*beta
 
             sampler.set_sample_function(sample_function)
@@ -85,8 +98,8 @@ class Sampler:
 
         """
         assert isinstance(sample_function, (types.FunctionType, types.BuiltinFunctionType)), 'sample_function must be a function'
-        dset = set(sample_function.__code__.co_varnames) - set(self.sampling_vars)
-        #assert len(dset) == 0, 'sample_function must only contain keyword arguments that appear as sample vars in the sampling_plan. You have the unknown arguments: {}'.format(dset)
+        dset = set(inspect.getargspec(sample_function).args) - set(self.sampling_vars)
+        assert len(dset) == 0, 'sample_function must only contain keyword arguments that appear as sample vars in the sampling_plan. You have the unknown arguments: {}'.format(dset)
 
         self.sample_function = sample_function
 
@@ -94,6 +107,9 @@ class Sampler:
 
 
     def _save_name(self, sample_id):
+        """Private method. Used in :py:meth:`sample_data`.
+        Creates the name for a given sample based on the sample plan name and the sample id.
+        """
         name = '{plan_name}_{id}'.format(plan_name=self.sampling_plan['name'], id=sample_id)
 
         if self.sampling_plan['save_format'] == 'pickle':
@@ -105,7 +121,10 @@ class Sampler:
 
 
     def _save(self, save_name, result):
-        if os.path.isfile(save_name):
+        """Private method. Saves the result for a single sample in the defined format.
+        Considers the ``overwrite`` parameter to check if existing results should be overwritten.
+        """
+        if os.path.isfile(save_name) or self.overwrite:
             None
         else:
             if self.sampling_plan['save_format'] == 'pickle':
@@ -115,6 +134,10 @@ class Sampler:
 
 
     def sample_data(self):
+        """Sample data after having configured the :py:class:`Sampler`.
+        No user input is required and the method will iterate through all the items defined in the ``sampling_plan`` (obtained with :py:class:SamplingPlanner).
+        Depending on your ``sample_function`` (set with :py:meth:`set_sample_function`) and the total number of samples, executing this method may take some time.
+        """
         assert self.flags['set_sample_function'], 'Cannot sample before setting the sample function with Sampler.set_sample_function'
 
         for i, sample in enumerate(self.sampling_plan['sampling_plan']):
@@ -132,3 +155,5 @@ class Sampler:
                 result = self.sample_function(**sample_i)
 
                 self._save(save_name, result)
+
+            printProgressBar(i, self.sampling_plan['n_samples'], prefix = 'Progress:', suffix = 'Complete', length = 50)
