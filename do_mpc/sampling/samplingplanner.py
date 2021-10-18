c300786f50c6c956282ed8f32f6f6e9cefcf1baa
Felix-Mac <felix.fiedler@tu-berlin.de>
2021-10-18 09:30:18 +0200

    SamplingPlanner Update
    - Main attribute does not carry meta information anymore. It is now just a list of dictionaries.
    - Can now set sampling_vars without function to sample from. This allows to manually add sampling cases
    - add_sampling_case method allows to add sampling case with user defined values. 
    - gen_sampling_plan does not export a file
    - export method used to export file

--- do_mpc/sampling/samplingplanner.py
+++ do_mpc/sampling/samplingplanner.py
@@ -11,22 +11,26 @@ from do_mpc.tools import load_pickle, save_pickle
 
 class SamplingPlanner:
     """A class for generating sampling plans.
-    These sampling plans will be executed by :py:class:`Sampler` to generate data
+    These sampling plans will be executed by :py:class:`do_mpc.sampling.Sampler` to generate data
     which can be used for evaluating the performance the performance of the considered configuration, machine learning, etc.
 
     **Configuration and sampling plan generation:**
 
     Configuring and generating a sampling plan involves the following steps:
 
-    1. Set variables which should be sampled with :py:func:`set_sampling_var`, e.g. the initial state.
+    1. Set variables which should be sampled with :py:func:`set_sampling_var`.
 
     2. (Optional) Set further options of the SamplingPlanner with :py:meth:`set_param`
+
+    You can then manually or automatically or as a mix of both create the sampling plan with:
 
     3. Generate the sampling plan with :py:func:`gen_sampling_plan`.
 
     """
     def __init__(self):
         self.sampling_vars = []
+        self.sampling_var_names = []
+        self.sampling_plan = []
 
         # Parameters that can be set for the SamplingPlanner:
         self.data_fields = [
@@ -36,9 +40,38 @@ class SamplingPlanner:
 
         self.overwrite = False
         self.save_format = 'pickle'
+        self.id_precision = 3
 
     def set_param(self, **kwargs):
-        """
+        """Set the parameters of the :py:class:`SamplingPlanner` class. Parameters must be passed as pairs of valid keywords and respective argument.
+        For example:
+
+        ::
+
+            sp.set_param(overwrite = True)
+
+        It is also possible and convenient to pass a dictionary with multiple parameters simultaneously as shown in the following example:
+
+        ::
+
+            setup_dict = {
+                'overwrite': True,
+                'save_format': pickle,
+            }
+            sp.set_param(**setup_dict)
+
+        This makes use of thy python "unpack" operator. See `more details here`_.
+
+        .. _`more details here`: https://codeyarns.github.io/tech/2012-04-25-unpack-operator-in-python.html
+
+        .. note:: Default parameters are available for most settings.
+
+        .. note:: :py:func:`set_param` can be called multiple times. Previously passed arguments are overwritten by successive calls.
+
+        The following parameters are available:
+
+        :param overwrite: Overwrites existing samplingplan under the same name, if set to ``True``.
+        :type overwrite: bool
 
         """
         for key, value in kwargs.items():
@@ -47,8 +80,12 @@ class SamplingPlanner:
             else:
                 setattr(self, key, value)
 
-    def set_sampling_var(self, name, fun_var_pdf):
-        """Introduce new sampling variables to the :py:class:`SamplingPlanner`. Define variable name and the function that generates a value for the corresponding variable.
+    def set_sampling_var(self, name, fun_var_pdf=None):
+        """Introduce new sampling variables to the :py:class:`SamplingPlanner`. Define variable name.
+        Optionally add a function to generate values for the sampled variable (e.g. following some distribution).
+        The parameter ``fun_var_pdf`` defaults to ``None`` and allows
+
+        and the function that generates a value for the corresponding variable.
 
         :param name: Declare the name of the variable
         :type name: string
@@ -59,42 +96,63 @@ class SamplingPlanner:
         :raises assertion: must be Function or BuiltinFunction_or_Method
         """
         assert isinstance(name, str), 'name must be str, you have {}'.format(type(name))
-        assert isinstance(fun_var_pdf, (types.FunctionType, types.BuiltinFunctionType)), 'fun_var_pdf must be either Function or BuiltinFunction_or_Method, you have {}'.format(type(fun_var_pdf))
+        assert isinstance(fun_var_pdf, (types.FunctionType, types.BuiltinFunctionType, type(None))), 'fun_var_pdf must be either Function or BuiltinFunction_or_Method or None, you have {}'.format(type(fun_var_pdf))
         self.sampling_vars.append({'name':name, 'fun_var_pdf':fun_var_pdf})
+        self.sampling_var_names.append(name)
 
-    def gen_sampling_plan(self, sampling_plan_name, n_samples):
+    def add_sampling_case(self, **kwargs):
+        """
+
+        """
+        # Create each sampling case as dict:
+        temp_dic = {}
+        # Iterate over all the key value pairs added in the method call:
+        for key, value in kwargs.items():
+            if not (key in self.sampling_var_names):
+                raise Exception('{} is not a valid sampling variable. Introduce sampling variables with set_sampling_var.'.format(key))
+            else:
+                temp_dic[key] = value
+
+        # Add key value pairs for all keys that were not referenced in add_sampling_case:
+        for var in self.sampling_vars:
+            if var['name'] not in kwargs.keys():
+                temp_dic[var['name']] = var['fun_var_pdf']()
+
+        # Generate string ID of sampling case based on index and pad with zeros:
+        id = len(self.sampling_plan)
+        temp_dic['id'] = str(id).zfill(self.id_precision)
+
+        self.sampling_plan.append(temp_dic)
+
+        return self.sampling_plan
+
+
+
+    def gen_sampling_plan(self, n_samples):
         """Generate the sampling plan. The generated sampling contains ``n_samples`` samples based on the defined variables and the corresponding evaluation functions.
 
-        :param sampling_plan_name: The name of the sampling plan.
-        :type sampling_plan_name: string
         :param n_samples: The number of generated samples
         :type n_samples: int
 
-        :raises assertion: sampling_plan_name must be string
         :raises assertion: n_samples must be int
 
         :return: Returns the newly created sampling plan.
         :rtype: list
         """
-        assert isinstance(sampling_plan_name, str), 'sampling_plan_name must be str, you have {}'.format(type(var_type))
         assert isinstance(n_samples, int), 'n_samples must be int, you have {}'.format(type(n_samples))
 
-        sampling_plan = []
-
         for i in range(n_samples):
+            self.add_sampling_case()
 
-            n_digits = len(str(n_samples))
 
-            temp_dic = {var['name']: var['fun_var_pdf']() for var in self.sampling_vars}
-            temp_dic.update({'id': str(i).zfill(n_digits)})
+        return self.sampling_plan
 
-            sampling_plan.append(temp_dic)
 
-        # save sampling plan
-        self.sampling_plan = {'n_samples':n_samples, 'save_format': self.save_format, 'sampling_plan':sampling_plan}
+    def export(self, sampling_plan_name):
+        """
 
+        """
         if not os.path.isfile(sampling_plan_name + '.pkl') or self.overwrite:
-            self.sampling_plan.update({'name': sampling_plan_name})
             save_pickle(sampling_plan_name, self.sampling_plan)
         else:
             for i in range(1,10000):
@@ -102,6 +160,3 @@ class SamplingPlanner:
                     self.sampling_plan.update({'name': sampling_plan_name + '_' + str(i)})
                     save_pickle(sampling_plan_name + '_' + str(i), self.sampling_plan)
                     break
-
-
-        return self.sampling_plan
