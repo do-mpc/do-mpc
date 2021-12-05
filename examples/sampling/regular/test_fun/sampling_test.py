7873acda496d144b41c0212043a8e2912c1d0d62
Felix-Mac <felix.fiedler@tu-berlin.de>
2021-12-05 17:55:49 +0100

    updated sampling_test

--- examples/sampling/regular/test_fun/sampling_test.py
+++ examples/sampling/regular/test_fun/sampling_test.py
@@ -7,39 +7,45 @@ import pdb
 import time
 
 
-np.random.seed(123)
+def main():
+    np.random.seed(123)
 
-sp = do_mpc.sampling.SamplingPlanner()
-sp.set_param(overwrite = True)
-sp.data_dir = './sample_results/'
-
-
-sp.set_sampling_var('alpha', np.random.randn)
-sp.set_sampling_var('beta', lambda: np.random.randint(0,5))
-
-_ = sp.gen_sampling_plan(n_samples=10)
-# Add custom cases:
-_ = sp.add_sampling_case(alpha=10)
-_ = sp.add_sampling_case(beta=10)
-plan = sp.add_sampling_case(alpha=2, beta=2)
-
-sampler = do_mpc.sampling.Sampler(plan)
-sampler.set_param(overwrite=True)
-sampler.data_dir = './sample_results/'
-
-def sample_function(alpha, beta):
-    time.sleep(0.1)
-    return alpha*beta
-
-sampler.set_sample_function(sample_function)
-
-sampler.sample_data()
-
-dh = do_mpc.sampling.DataHandler(plan)
-dh.data_dir='./sample_results/'
-
-dh.set_post_processing('res_1', lambda x: x)
-dh.set_post_processing('res_2', lambda x: x**2)
+    sp = do_mpc.sampling.SamplingPlanner()
+    sp.set_param(overwrite = True)
+    sp.data_dir = './sample_results/'
 
 
-res = dh[:]
+    sp.set_sampling_var('alpha', np.random.randn)
+    sp.set_sampling_var('beta', lambda: np.random.randint(0,5))
+
+    _ = sp.gen_sampling_plan(n_samples=10)
+    # Add custom cases:
+    _ = sp.add_sampling_case(alpha=10)
+    _ = sp.add_sampling_case(beta=10)
+    plan = sp.add_sampling_case(alpha=2, beta=2)
+
+    sampler = do_mpc.sampling.Sampler(plan)
+    sampler.set_param(overwrite=True)
+    sampler.data_dir = './sample_results/'
+
+    def sample_function(alpha, beta):
+        time.sleep(0.1)
+        return alpha*beta
+
+    sampler.set_sample_function(sample_function)
+
+    sampler.sample_data()
+
+    dh = do_mpc.sampling.DataHandler(plan)
+    dh.data_dir='./sample_results/'
+
+    dh.set_post_processing('res_1', lambda x: x)
+    dh.set_post_processing('res_2', lambda x: x**2)
+
+
+    res = dh[:]
+
+
+
+if __name__ == '__main__':
+    main()
