f0cade469324046dcfcadaf13b87c8d0aebbeadd
Felix-Mac <felix.fiedler@tu-berlin.de>
2023-01-23 13:08:00 +0100

    Modifications of LQR to store data

--- do_mpc/controller/_lqr.py
+++ do_mpc/controller/_lqr.py
@@ -22,14 +22,16 @@
 #   along with do-mpc.  If not, see <http://www.gnu.org/licenses/>.
 
 import casadi
+from casadi.tools import *
 import numpy as np
 import warnings
 import pdb
 
+import do_mpc.data
 from scipy.linalg import solve_discrete_are
-from ..model import LinearModel
+from ..model import LinearModel, IteratedVariables
 
-class LQR:
+class LQR(IteratedVariables):
     """Linear Quadratic Regulator.
     Use this class to configure and run the LQR controller
     according to the previously configured :py:class:`do_mpc.model.Model` instance. 
@@ -80,17 +82,21 @@ class LQR:
     """
     def __init__(self,model):
         self.model = model
+        IteratedVariables.__init__(self)
         
         assert isinstance(model, LinearModel), 'LQR can only be used with linear models. Initialize the model with LinearModel class.'
         assert model.flags['setup'] == True, 'Model for LQR was not setup. After the complete model creation call model.setup().'
         assert model.model_type == 'discrete', 'Initialize LQR with discrete system. Discretize the system using LinearModel.discretize()'
         
         self.model_type = model.model_type
+
+        self.data = do_mpc.data.Data(model)
         
         #Parameters necessary for setting up LQR
         self.data_fields = [
             'n_horizon',
             'mode',
+            't_step',
             ]
         #Initialize prediction horizon for the problem
         self.n_horizon = None
@@ -99,11 +105,16 @@ class LQR:
         self.mode = 'setPointTrack'
         
         self.flags = {'setup':False}
-        
-        self.u0 = np.array([[]])
+
         
         self.xss = None
         self.uss = None
+
+    def reset_history(self):
+        """Reset the history of the LQR.
+        """
+        self._t0 = np.array([0])
+        self.data.init_storage()
         
     def discrete_gain(self,A,B):
         """Computes discrete gain. 
@@ -292,29 +303,47 @@ class LQR:
         """
         #verify setup of lqr is done
         assert self.flags['setup'] == True, 'LQR is not setup. run setup() function.'
+        
+        # Check input type.
+        if isinstance(x0, (np.ndarray, casadi.DM)):
+            pass
+        elif isinstance(x0, structure3.DMStruct):
+            x0 = x0.cat
+        else:
+            raise Exception('Invalid type {} for x0. Must be {}'.format(type(x0), (np.ndarray, casadi.DM, structure3.DMStruct)))
 
         #setting setpoints
         if self.xss is None and self.uss is None:
             self.set_setpoint()
         
-        #Initializing u0
-        if self.u0.size == 0:
-            self.u0 = np.zeros((self.model.n_u,1))
         
         #Calculate u in set point tracking mode
         if self.mode == "setPointTrack":
             if self.xss.size != 0 and self.uss.size != 0:
-                self.u0 = self.K@(x0-self.xss)+self.uss
+                u0 = self.K@(x0-self.xss)+self.uss
         
         #Calculate u in input rate penalization mode
         elif self.mode == "inputRatePenalization":
             if np.shape(self.K)[1]==np.shape(x0)[0]:
-                self.u0 = self.K@(x0-self.xss)+self.uss
-                self.u0 = self.u0+x0[-self.model.n_u:]
+                u0 = self.K@(x0-self.xss)+self.uss
+                u0 = self.u0+x0[-self.model.n_u:]
             elif np.shape(self.K)[1]!=np.shape(x0)[0] and np.shape(self.K)[1]== np.shape(np.block([[x0],[self.u0]]))[0]:
                 x0_new = np.block([[x0],[self.u0]])
-                self.u0 = self.K@(x0_new-self.xss)+self.uss
-                self.u0 = self.u0+x0_new[-self.model.n_u:]
+                u0 = self.K@(x0_new-self.xss)+self.uss
+                u0 = self.u0+x0_new[-self.model.n_u:]
+
+
+        t0 = self._t0
+        # Store solution:
+        self.data.update(_x = x0)
+        self.data.update(_u = u0)
+        self.data.update(_time = t0)
+
+        # Update initial
+        self._t0 = self._t0 + self.t_step
+        self._x0.master = x0
+        self._u0.master = u0
+        
         return self.u0
         
     def set_objective(self, Q = None, R = None, P = None, Rdelu = None):
@@ -443,8 +472,8 @@ class LQR:
         :raises exception: mode must be setPointTrack, inputRatePenalization, None. you have {string value}
 
         """
-        if self.n_horizon == None:
-            warnings.warn('discrete infinite horizon gain will be computed since prediction horizon is set to default value 0')
+        assert self.t_step, 't_step is required in order to setup the LQR. Please set the simulation time step via set_param(**kwargs)'
+
         if self.mode in ['setPointTrack',None]:
             self.K = self.discrete_gain(self.model._A,self.model._B)
         elif self.mode == 'inputRatePenalization':
