edfb64eaccf90a3133712bf75cedc4622439d75e
David Zanger <david.zanger@dlr.de>
2023-04-03 15:27:32 +0200

    Fix bug in algebraic variable initializer. There was not substitution for p, tvp, x and u.

--- do_mpc/tools/algebraic_variable_initializer.py
+++ do_mpc/tools/algebraic_variable_initializer.py
@@ -1,14 +1,16 @@
 """Module to initialize the algebraic variables."""
+from typing import Any
+
 import casadi as cd
 
-from do_mpc.model import IteratedVariables, Model
+from do_mpc.model import Model
 
 SUPPRESS_IPOPT = {'ipopt.print_level': 0, 'ipopt.sb': 'yes', 'print_time': 0}
 
 
 def init_algebraic_variables(
     model: Model,
-    obj: IteratedVariables,
+    obj: Any,
 ) -> None:
     """Initializes the algebraic variables.
 
@@ -27,7 +29,15 @@ def init_algebraic_variables(
     z0 = cd.vertcat(obj.z0)
     residual_to_initial_guess = cd.vertcat(model.z) - z0
     nlp['f'] = cd.sum2(cd.sum1(residual_to_initial_guess**2))
-    nlp['g'] = cd.vertcat(model._alg)
+    algebraic_equations = cd.vertcat(model._alg)
+    variables_to_substitute = cd.vertcat(model.p, model.tvp, model.u, model.x)
+    initial_guess = cd.vertcat(obj.p_fun(obj.t0),
+                               obj.tvp_fun(obj.t0), obj.u0,
+                               obj.x0)
+    initialized_algebraic_equations = cd.substitute(algebraic_equations,
+                                                    variables_to_substitute,
+                                                    initial_guess)
+    nlp['g'] = initialized_algebraic_equations
 
     solver = cd.nlpsol("solver", "ipopt", nlp, SUPPRESS_IPOPT)
     res = solver(x0=z0, lbg=0, ubg=0)
