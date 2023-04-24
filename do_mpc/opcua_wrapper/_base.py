59e41d027c6aa7e11757f32a34b78a85b15a9908
Mrfeli01 <88486457+Mrfeli01@users.noreply.github.com>
2023-04-24 16:19:54 +0200

    Added documentation to RTBase

--- do_mpc/opcua_wrapper/_base.py
+++ do_mpc/opcua_wrapper/_base.py
@@ -31,8 +31,48 @@ import casadi.tools as ctools
 
         
 class RTBase:
+    ''' Real Time Base.
+    The RTBase class extends the :py:class:`do_mpc.controller.MPC` and the :py:class:`do_mpc.Simulator` classes with an OPC UA interface.
+
+    Use this class to configure a OPC UA client with previously initiated :py:class:`do_mpc.controller.MPC` or :py:class:`do_mpc.Simulator` instances.
+
+    **Configuration and setup:**
+
+    Configuring and setting up the MPC controller involves the following steps:
+
+    1. Use :py:class:`do_mpc.opcua_wrapper.ClientOpts` dataclass to specify client name as well as IP adress and port of the target server.
+
+    2. Initiate the RTBase class with a do-mpc object and the dataclass :py:class:`do_mpc.opcua_wrapper.ClientOpts`.
+
+    3. Use :py:meth:`set_write_tags` and :py:meth:`set_read_tags` to take over the tags from another instance of RTBase (optional).
+
+    4. Use :py:meth:`connect` to connect the clienet to the OPC UA server.
+
+    4. Use :py:meth:`write_to_tags` to write the initial values to the OPC UA server.
+
+    5. Use :py:meth:`async_step_start` to run the do-mpc :py:meth:`do_mpc.MPC.make_step`.
+
+    .. note::
+
+        This class currently doesent work with the :py:class:`do_mpc.estimator.MHE` class.
+    
+    '''
 
     def __init__(self, do_mpc_object, clientOpts, namespace=None):
+        
+        """
+        Initializes the real-time base object with the provided do_mpc object, client options, and an optional namespace.
+        
+        :param do_mpc_object: The initiated instance of a do-mpc class. Currently supported are :py:class:`do_mpc.controller.MPC` nd :py:class:`do_mpc.Simulator`
+        :type: class
+
+        :param clientOpts: A dataclass specifying the client name , IP address and port for the target server. See :py:class:`do_mpc.opcua_wrapper.ClientOpts` for further information.
+        :dtype: dataclass
+
+        :param namespace: A dataclass specifying tags for the OPC UA namespace (optional). If no namespace dataclass is given, a default namespace is generated from the do-mpc object. See :py:class:`do_mpc.opcua_wrapper.Namespace` for further information.
+        :type: dataclass
+        """
+
         self.do_mpc_object = do_mpc_object
 
         if namespace == None:
@@ -49,23 +89,44 @@ class RTBase:
 
 
     def namespace_from_model(self, model, model_name):
+
+        """
+        Create a namespace from the provided model.
+        
+        :param model: A do-mpc model :py:class:`do_mpc.model.Model`
+        :type model: class
+
+        :param model_name: Name given to the generated namespace.
+        :type model_name: str
+
+        """
+
         node_list = []
         variable_list = ['aux', 'p', 'tvp', 'u', 'v', 'w', 'x', 'y', 'z']
+
         for var in variable_list:
+            
             for key in model[var].labels():
-                if key.strip('[]').split(',')[0] != 'default':
-                    node_list.append(NamespaceEntry(var, key))
-                else:
+                if 'default' in key:
                     continue
+                
+                node_list.append(NamespaceEntry(var, key))
 
         return Namespace(model_name, node_list)
 
 
     def get_default_namespace(self, namespace_name):
+        '''
+        Set default namespace using :py:meth:`namespace_from_model`.
+        '''
         self.def_namespace = self.namespace_from_model(self.do_mpc_object.model, namespace_name)
 
 
     def connect(self):
+        '''
+        Connect client to server.
+        '''
+
         try:
             self.client.connect()
         except RuntimeError:
@@ -73,6 +134,10 @@ class RTBase:
 
 
     def disconnect(self):
+        '''
+        Disconnect client from server.
+        '''
+
         try:
             self.client.disconnect()
         except RuntimeError:
@@ -80,20 +145,42 @@ class RTBase:
 
 
     def set_write_tags(self,tagout:List[str]):
+        '''
+        Set tags to write to. The provided tags must match the variable names registered on the taget server.
+
+        :param tagout: A list of strings specifying the variables the client is writing.
+        :type tagout: List[str]
+        '''
         self.tagout = tagout
 
 
+
     def set_read_tags(self, tagin:List[str]):
+        '''
+        Set tags to read from. The provided tags must match the variable names registered on the taget server.
+
+        :param tagout: A list of strings specifying the variables the client is reading from.
+        :type tagout: List[str]
+        '''
         self.tagin = tagin
 
 
     def make_step(self):
+        '''
+        The do-mpc make_step method e.g. :py:meth:`do_mpc.MPC.make_step`
+        '''
         input = self.read_from_tags()
         output = self.do_mpc_object.make_step(input)
         self.write_to_tags(output)
 
 
     def write_to_tags(self, data):
+        '''
+        Write results of :py:meth:`make_step` to server.
+
+        :param data: data which is written to server.
+        :type data: ctools.structure3.DMStruct, ctools.DM, np.ndarray
+        '''
         if isinstance(data, ctools.structure3.DMStruct):
             data = data.cat.full().flatten()
         elif isinstance(data, ctools.DM):
@@ -112,6 +199,9 @@ class RTBase:
 
 
     def read_from_tags(self):
+        '''
+        Read current values from server.
+        '''
         return np.array([self.client.readData(i) for i in self.tagin]).reshape(-1,1)
 
 
@@ -122,19 +212,25 @@ class RTBase:
 
 
     def async_step_start(self):
+        '''
+        Call the :py:meth:`make_step` method in a frequency given by the do-mpc classes t_step value.
+        '''
         if self.new_init == True:
             self.new_thread = Thread(target=self.make_step)
             self.new_thread.start()
             self.new_init = False
 
         if not self.is_running:
-            self.cycle = time.time() + self.cycle_time
+            self.cycle = time.time() + self.cycle_time 
             self.thread = Timer(self.cycle - time.time(), self.async_run)
             self.thread.start()
             self.is_running = True
 
 
     def async_step_stop(self):
+        '''
+        Stop :py:meth:`async_step_start` from running.
+        '''
         self.thread.cancel()
         self.is_running = False
         self.new_init = True
@@ -162,8 +258,6 @@ class Namespace:
 
     def __getitem__(self, nodename: str):
         return [entry.get_node_id(self._namespace_index) for entry in self.entry_list if entry.objectnode == nodename]
-
-
 
 
 
--- do_mpc/opcua_wrapper/_server.py
+++ do_mpc/opcua_wrapper/_server.py
@@ -48,28 +48,45 @@ class RTServer:
             print("Server could not be created. Check your opcua module installation!")
             return False
 
-        
+
+    # Register namespace from client    
     def namespace_from_client(self, client):
+        # get namespace from clients method
         client_namespace = client.client.namespace
         self.object_node_dict = {}
+        # register a new namespace on the OPC UA server
         idx = self.opcua_server.register_namespace(client_namespace.namespace_name)
 
+        # iterate through client namespace
         for namespace_entry in client_namespace.entry_list: 
-            if namespace_entry.objectnode not in self.object_node_dict.keys():
+            # check for every object node if it was already registered
+            if namespace_entry.objectnode not in self.object_node_dict:
+                # if not, register object node 
                 object = self.opcua_server.nodes.objects.add_object(idx, namespace_entry.objectnode)
                 self.object_node_dict[namespace_entry.objectnode] = object
 
+            # add variable to object node
             self.add_variable_to_node(namespace_entry, idx)
 
-        client.client.add_namespace_url(idx)     
-        print(f"The following namespaces are registered: {self.opcua_server.get_namespace_array()[2:]}")
+        # write namespace index to client namespace for clients read/write methods
+        client.client.add_namespace_url(idx)
+        namespace_array = self.opcua_server.get_namespace_array()[2:]     
+        print(f"The following namespaces are registered: {namespace_array}")
 
 
-    def add_variable_to_node(self, NamespaceEntry, Namespace_url):
-        variable_name = f'{NamespaceEntry.objectnode}{NamespaceEntry.variable}'
-        datavector = self.object_node_dict[NamespaceEntry.objectnode].add_variable(opcua.ua.NodeId(variable_name, Namespace_url), variable_name, [0.0])
+    # Add variables to a registered node
+    def add_variable_to_node(self, namespace_entry, namespace_url):
+        # create unique and descriptive variable name
+        variable_name = f'{namespace_entry.objectnode}{namespace_entry.variable}'
+        # add variable to object node
+        datavector = self.object_node_dict[namespace_entry.objectnode].add_variable(
+            opcua.ua.NodeId(variable_name, namespace_url), 
+            variable_name, 
+            [0.0]
+            )
+        
         datavector.set_writable()
-        NamespaceEntry.variable = variable_name
+        namespace_entry.variable = variable_name
 
     
     # Start server
