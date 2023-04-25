a17ed1d094b0e3d3c534df5f1c127c2fa964e7c5
Felix-Mac <felix.fiedler@tu-berlin.de>
2023-04-25 12:03:26 +0200

    test_batch_reactor_lqr_dae updated

--- testing/test_batch_reactor_lqr_dae.py
+++ testing/test_batch_reactor_lqr_dae.py
@@ -112,10 +112,13 @@ class TestBatchReactorLQRDAE(unittest.TestCase):
 
             test = ['_x', '_u', '_time', '_z']
             
+            msg = 'Check if variable {var} for {module} is identical to previous runs: {check}. Max diff is {max_diff:.4E}.'
             for test_i in test:
+
                 # Check Simulator
-                check = np.allclose(simulator.data.__dict__[test_i], ref['simulator'].__dict__[test_i])
-                self.assertTrue(check)
+                max_diff = np.max(np.abs(simulator.data.__dict__[test_i] - ref['simulator'].__dict__[test_i]), initial=0)
+                check = max_diff < 1e-8
+                self.assertTrue(check, msg.format(var=test_i, module='Simulator', check=check, max_diff=max_diff))
                 
             # Store for test reasons
             try:
