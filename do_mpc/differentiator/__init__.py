d87d96f6bdc4cc41ae0f613c27db711bb0763e73
Felix-Mac <felix.fiedler@tu-berlin.de>
2023-05-02 21:57:57 +0200

    Updated import of nlpdifferentiator

--- do_mpc/differentiator/__init__.py
+++ do_mpc/differentiator/__init__.py
@@ -1,11 +1,10 @@
 """
 Tools for NLP differentiation.
 
-.. warning::
+Warning:
     This module is not fully implemented and experimental.
 """
 
 # from ._nlphandler import NLPHandler
 from ._nlpdifferentiator import NLPDifferentiator, DoMPCDifferentiatior
-# from ._nlpdifferentiator import build_sens_sym_struct
-# from ._nlpdifferentiator import assign_num_to_sens_struct
+from . import helper
