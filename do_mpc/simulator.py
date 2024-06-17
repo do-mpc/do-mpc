ec167527f7a14f231b2fcf4a2cb6cad6f74c079b
Mrfeli01 <88486457+Mrfeli01@users.noreply.github.com>
2024-06-17 14:44:01 +0200

    Update simulator.py
    Fixed minor bug

--- do_mpc/simulator.py
+++ do_mpc/simulator.py
@@ -282,6 +282,7 @@ class Simulator(do_mpc.model.IteratedVariables):
                 'alg': alg,
             }
 
+            opts = {}
             # Set the integrator options, note that 'abstol' and 'reltol' are not needed for collocation
             if self._settings.integration_tool != 'collocation':
                 opts = {
