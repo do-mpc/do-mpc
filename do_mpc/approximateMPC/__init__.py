91fe3f241a26674fbc57355bbf197bb23db4a207
Mrfeli01 <88486457+Mrfeli01@users.noreply.github.com>
2025-03-27 11:41:51 +0100

    Update __init__.py
    Added documentation for more descriptive html files.

--- do_mpc/approximateMPC/__init__.py
+++ do_mpc/approximateMPC/__init__.py
@@ -1,3 +1,7 @@
+'''
+Neural network approxmation of the model predictive controller.
+'''
+
 from ._trainer import Trainer
 from ._ampc import ApproxMPC, FeedforwardNN
 from ._sampling import Sampler
