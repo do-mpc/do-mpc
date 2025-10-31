365343abdc618f6619a07289cc0cdae60d9bcbe3
Mrfeli01 <88486457+Mrfeli01@users.noreply.github.com>
2025-10-31 09:09:13 +0100

    Fix warning message formatting in __init__.py

--- do_mpc/approximateMPC/__init__.py
+++ do_mpc/approximateMPC/__init__.py
@@ -10,4 +10,4 @@ if __TORCH_INSTALLED__:
     from ._ampc import ApproxMPC, FeedforwardNN, ApproximateMPCSettings
     from ._ampc_sampler import AMPCSampler
 else:
-    warnings.warn('The approximateMPC feature requires PyTorch, which is not installed by default. Please install the full version of do-mpc to use this feature: pip install do-mpc[full]')
+    warnings.warn('The approximateMPC feature requires PyTorch, which is not installed by default. Please install the full version of do-mpc to use this feature: pip install do_mpc[full]')
